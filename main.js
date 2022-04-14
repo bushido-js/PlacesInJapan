78cdd944a588398717710c2b59452b2d8d012085
Oleg Bidikhov <olbid99@gmail.com>
2022-04-15 00:21:24 +0300

    JapanPage version 1.0

---
+++ main.js
@@ -0,0 +1,21 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active')
+        } else {
+            header.classList.remove('header_active')
+        }
+    }
+}());
+
+
+
+// Burger handler
+
+const headerNav = document.querySelector('.header__nav')
+
+
+const burger = document.querySelector('.burger').addEventListener('click', () => headerNav.classList.add('header__nav_active'))
+
+const burgerClose = document.querySelector('.header__nav-close').addEventListener('click', () => headerNav.classList.remove('header__nav_active'))
